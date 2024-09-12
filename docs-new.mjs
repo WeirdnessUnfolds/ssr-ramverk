bf237e0361d0973e2f1cbee0c5d0a9243843d34d
Viktoria Arvidsson <viarven@hotmail.com>
2024-09-12 11:28:17 +0200

    Added more test-functions

--- docs-new.mjs
+++ docs-new.mjs
@@ -8,11 +8,34 @@ const docs = {
             const client  = await mongo.connect(dsn);
             const db = await client.db();
             const col = await db.collection(crowd);
-            const res = await col.find().toArray();
+            const res = await col.find({}).toArray();
         
             await client.close();
         
             return res;
+    },
+
+    findWithId: async function findWithId(id) {
+        const client  = await mongo.connect(dsn);
+        const db = await client.db();
+        const col = await db.collection(crowd);
+        const res = await col.find({_id:id}).toArray();
+    
+        await client.close();
+    
+        return res;
+    },
+
+    addDocument: async function addDocument(title, content) {
+        const client  = await mongo.connect(dsn);
+        const db = await client.db();
+        const col = await db.collection(crowd);
+        const doc = {title: title, content: content}
+        const res = await col.insertOne(doc);
+    
+        await client.close();
+    
+        return res;
     },
     
 }
