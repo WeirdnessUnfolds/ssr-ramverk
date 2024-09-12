4e70996c60b0f711b8fee44280c1c5b475816126
Viktoria Arvidsson <viarven@hotmail.com>
2024-09-12 10:46:52 +0200

    find-all function test

--- docs-new.mjs
+++ docs-new.mjs
@@ -3,3 +3,16 @@
 const mongo = require("mongodb").MongoClient;
 const dsn =  process.env.DBWEBB_DSN || "mongodb://localhost:27017/docs";
 
+const docs = {
+    findAll: async function findAll() {
+            const client  = await mongo.connect(dsn);
+            const db = await client.db();
+            const col = await db.collection(crowd);
+            const res = await col.find().toArray();
+        
+            await client.close();
+        
+            return res;
+    },
+    
+}
