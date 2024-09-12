8c9e021807f3562bbb993f8d4bd9c72f8f9896e6
Viktoria Arvidsson <viarven@hotmail.com>
2024-09-12 15:38:27 +0200

    Added comment on test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+
 describe('Array', function () {
     describe('#indexOf()', function () {
       it('should return -1 when the value is not present', function () {
@@ -6,3 +7,16 @@ describe('Array', function () {
       });
     });
   });
+
+// Exempel på att testea med async/await
+//   beforeEach(async function () {
+//     await db.clear();
+//     await db.save([tobi, loki, jane]);
+//   });
+  
+//   describe('#find()', function () {
+//     it('responds with matching records', async function () {
+//       const users = await db.find({type: 'User'});
+//       users.should.have.length(3);
+//     });
+//   });
