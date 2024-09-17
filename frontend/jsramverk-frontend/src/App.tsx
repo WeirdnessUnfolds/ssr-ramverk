3d12ab059acb8e5acbd444f1208ad06f526c9253
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2024-09-17 15:53:35 +0200

    Implemented loading status

--- frontend/jsramverk-frontend/src/App.tsx
+++ frontend/jsramverk-frontend/src/App.tsx
@@ -1,21 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 function App() {
   
   const [items, setItems] = useState([])
-  axios.get("http://localhost:3539").then((res) => {
-    setItems(res.data)
-  });
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    axios.get("http://localhost:3539").then((res) => {
+      setItems(res.data);
+      setLoading(false);
+    });
+  }, []);
+
 
   return (
     <>
       <div className="navbar">
         <p>Navbar items</p>
       </div>
+     {
+     loading ?  
+      <div>
+        <p>Loading...</p>
+      </div>
+      :
       <div className = "doclist">
         <p>{items.map((item) => <li>{item.title}</li>)}</p>
       </div>
+}
     </>
   )
 }
