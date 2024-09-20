54e290e5ce9d02624abd1f049e83aa100dd299f8
Viktoria Arvidsson <viav23@student.bth.se>
2024-09-20 13:53:21 +0200

    Alert css added

--- frontend/jsramverk-frontend/src/views/Editdoc.tsx
+++ frontend/jsramverk-frontend/src/views/Editdoc.tsx
@@ -10,21 +10,20 @@ import { useState } from 'react'
 // Updates the selected document
 
 const EditDocview = ({ data, loading }: { data: Item; loading: boolean }) => {
-    const [alertVisible, setAlertVisibility] = useState(false)
+    const [alertVisible, setAlertVisibility] = useState(true)
     const handleClick = () => {
         axios.put(`http://localhost:3539/update/${data._id}`, data)
         setAlertVisibility(true)
     }
     return (
-        <>
 
-            loading ?
+        loading ?
             <div>
                 <p>Loading document contents..</p>
             </div>
             :
             <div>
-                {alertVisible && <Alert />}
+                {alertVisible && <Alert onClose={() => setAlertVisibility(false)} />}
                 <form className="docForm">
                     <label>Titel</label>
                     <input name="title" type="text" defaultValue={data.title}></input>
@@ -33,7 +32,7 @@ const EditDocview = ({ data, loading }: { data: Item; loading: boolean }) => {
                     <button type="submit"><FontAwesomeIcon icon={faFloppyDisk} onClick={handleClick} /></button>
                 </form>
             </div>
-        </>
+
     )
 }
 
