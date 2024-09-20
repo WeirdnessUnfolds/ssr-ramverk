4b2e5df5e0a4f1e7a6504ce8824f6e2042a14dc9
Viktoria Arvidsson <viav23@student.bth.se>
2024-09-20 13:24:23 +0200

    Alert added

---
+++ frontend/jsramverk-frontend/src/views/Alert.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alert = () => {
+    return (
+        <>
+            <div className="alert">Nu är dokumentet sparat</div>
+        </>
+    )
+}
+
+export default Alert
--- frontend/jsramverk-frontend/src/views/Editdoc.tsx
+++ frontend/jsramverk-frontend/src/views/Editdoc.tsx
@@ -1,29 +1,38 @@
 import { Item } from './ShowAll'
+import Alert from './Alert'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios'
+import { useState } from 'react'
 
 // Ordna en action som uppdaterar
 
 // Updates the selected document
 
 const EditDocview = ({ data, loading }: { data: Item; loading: boolean }) => {
+    const [alertVisible, setAlertVisibility] = useState(false)
     const handleClick = () => {
         axios.put("http://localhost:3539", data)
-      }
+        setAlertVisibility(true)
+    }
     return (
-        loading ?
+        <>
+            {alertVisible && <Alert />}
+            loading ?
             <div>
                 <p>Loading document contents..</p>
             </div>
             :
-            <form className="docForm">
-                <label>Titel</label>
-                <input name="title" type="text" defaultValue={data.title}></input>
-                <label>Innehåll</label>
-                <textarea name="content" defaultValue={data.content}></textarea>
-                <button type="submit"><FontAwesomeIcon icon={faFloppyDisk} onClick={handleClick}/></button>
-            </form>
+            <div>
+                <form className="docForm">
+                    <label>Titel</label>
+                    <input name="title" type="text" defaultValue={data.title}></input>
+                    <label>Innehåll</label>
+                    <textarea name="content" defaultValue={data.content}></textarea>
+                    <button type="submit"><FontAwesomeIcon icon={faFloppyDisk} onClick={handleClick} /></button>
+                </form>
+            </div>
+        </>
     )
 }
 
