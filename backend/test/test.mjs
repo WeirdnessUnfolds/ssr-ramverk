42f7ef8f98677d0a6230a3effc8cd7193a011c5e
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2024-09-23 15:06:05 +0200

    Changed result length test to actual docs length

--- backend/test/test.mjs
+++ backend/test/test.mjs
@@ -7,7 +7,7 @@ describe('GET operations for database', () => {
     it('Findall() returns all documents in the test database', (done) => {
       console.log(dbhandler.findAll())
       dbhandler.findAll().then((result) => {
-        expect(result.length).to.be.greaterThan(5);
+        expect(result.length).to.be.greaterThan(3);
         done();
     }).catch((error) => {
       done(error);
