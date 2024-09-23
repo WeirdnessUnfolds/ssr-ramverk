aaa8d470594e2521ea2368475787a169be51c0b4
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2024-09-23 15:05:21 +0200

    Started proper testfile with tests now working

---
+++ backend/test/test.mjs
@@ -0,0 +1,16 @@
+import {  expect } from 'chai'
+import dbhandler from '../docs-new.mjs';
+
+
+
+describe('GET operations for database', () => {
+    it('Findall() returns all documents in the test database', (done) => {
+      console.log(dbhandler.findAll())
+      dbhandler.findAll().then((result) => {
+        expect(result.length).to.be.greaterThan(5);
+        done();
+    }).catch((error) => {
+      done(error);
+    });
+  });
+});
