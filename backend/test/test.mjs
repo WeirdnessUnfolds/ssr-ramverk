fd49c4693a58b3205592845b56d77e2cf6223961
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2024-09-23 15:34:52 +0200

    Npm script for pretest, resetting of test database

--- backend/test/test.mjs
+++ backend/test/test.mjs
@@ -5,9 +5,8 @@ import dbhandler from '../docs-new.mjs';
 
 describe('GET operations for database', () => {
     it('Findall() returns all documents in the test database', (done) => {
-      console.log(dbhandler.findAll())
       dbhandler.findAll().then((result) => {
-        expect(result.length).to.be.greaterThan(3);
+        expect(result.length).to.be.equal(3);
         done();
     }).catch((error) => {
       done(error);
