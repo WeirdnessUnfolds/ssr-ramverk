7b6a250ed1c77645ed26789de97997e8e90023af
Viktoria Arvidsson <viav23@student.bth.se>
2024-09-26 16:41:58 +0200

    test async

--- frontend/jsramverk-frontend/src/__tests__/App.test.tsx
+++ frontend/jsramverk-frontend/src/__tests__/App.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import App from "../App"
 import axios from 'axios'
 
@@ -16,6 +16,15 @@ test("should render loading message", () => {
     render(<App />);
     const loadingText = screen.getByText("Loading...");
     expect(loadingText).toBeInTheDocument();
+});
+
+test("should fetch and display asynchronous posts", async () => {
+    render(<App />);
+    screen.debug(); // text initially not present
+    await waitFor(() => expect(screen.getByText("Dokument")).toBeInTheDocument(), {
+        timeout: 2000,
+    });
+    screen.debug(); // text is present
 });
 
 // test("Renders doclist", async () => {
