7fa1a4a465e65e32f1f2451d0f116f9e7d7e9169
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2024-09-27 11:13:41 +0200

    Removed unnessecary import

--- frontend/jsramverk-frontend/src/__tests__/App.test.tsx
+++ frontend/jsramverk-frontend/src/__tests__/App.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
 import { fireEvent, render, screen, waitFor } from "@testing-library/react"
 import App from "../App"
-import axios from 'axios'
+
 
 beforeEach(() => {
     render(<App />);
