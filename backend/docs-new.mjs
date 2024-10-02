70076d607f76c57bf356833884b3a2592a9751e8
Viktoria Arvidsson <viav23@student.bth.se>
2024-10-02 15:54:09 +0200

    all tests running

--- backend/docs-new.mjs
+++ backend/docs-new.mjs
@@ -3,9 +3,8 @@ import 'dotenv/config';
 
 import { MongoClient, ObjectId } from 'mongodb';
 const mongo = MongoClient;
-let dsn = `mongodb+srv://${process.env.DB_USER}
-:${process.env.DB_PASS}@jsramverk.owzo2.mongodb.net/?retryWrites=true&w=majority&appName=jsramverk`;
-let collection = "docscollection";
+let dsn = "";
+let collection = "";
 
 if (process.env.NODE_ENV === 'test') {
     dsn = "mongodb://localhost:27017/testdocs";
@@ -14,6 +13,11 @@ if (process.env.NODE_ENV === 'test') {
 else if (process.env.NODE_ENV === 'integration-test') {
     dsn = "mongodb://localhost:27017/int-testdocs";
     collection = "int-testcollection";
+}
+else {
+    dsn = `mongodb+srv://${process.env.DB_USER}
+:${process.env.DB_PASS}@jsramverk.owzo2.mongodb.net/?retryWrites=true&w=majority&appName=jsramverk`;
+    collection = "docscollection";
 }
 
 const dbhandler = {
@@ -91,8 +95,8 @@ const dbhandler = {
         const client = await mongo.connect(dsn);
         const db = await client.db();
         const col = await db.collection(collection);
-        const res = await col.updateOne({_id: new ObjectId(id)},
-            { $set: {title: title, content: content }});
+        const res = await col.updateOne({ _id: new ObjectId(id) },
+            { $set: { title: title, content: content } });
 
         await client.close();
         return res;
