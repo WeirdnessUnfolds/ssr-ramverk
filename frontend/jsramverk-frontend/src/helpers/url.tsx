64e031c572cc7206f5cf97d9f33782fb6d43b48b
Viktoria Arvidsson <viav23@student.bth.se>
2024-10-10 14:42:35 +0200

    Changed url temp

--- frontend/jsramverk-frontend/src/helpers/url.tsx
+++ frontend/jsramverk-frontend/src/helpers/url.tsx
@@ -1,3 +1,3 @@
-export const url = process.env.NODE_ENV === 'integration-test'
+export const url = process.env.NODE_ENV === 'dev'
    ? "http://localhost:3539"
    : "https://jsramverk-eafmccbgceegf9bt.swedencentral-01.azurewebsites.net"
