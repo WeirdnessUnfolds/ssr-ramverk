447520702a36f6f47de1831730cafe46ae74002b
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2024-10-15 16:16:47 +0200

    Added toke check to findAll

--- backend/routes/baseroutes.mjs
+++ backend/routes/baseroutes.mjs
@@ -1,9 +1,10 @@
 import dbhandler from '../docs-new.mjs';
 import express from 'express';
+import checkToken from './middleware/checkToken.mjs';
 var router = express.Router();
 
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res, next) => checkToken(req, res, next), async (req, res) => {
     dbhandler.findAll().then(result => res.json(result))
         .catch(err => console.log(err));
 });
