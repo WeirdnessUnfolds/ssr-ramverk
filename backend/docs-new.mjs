b40cbfc1873ffaae327b5b7ad9a608c983e47fc6
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2024-10-18 16:18:52 +0200

    More refactoring for better coverage

--- backend/docs-new.mjs
+++ backend/docs-new.mjs
@@ -2,36 +2,18 @@
 import 'dotenv/config';
 import bcrypt from 'bcrypt';
 import { MongoClient, ObjectId } from 'mongodb';
+import { dsn, collection } from './dbenvs.mjs';
 const mongo = MongoClient;
-let dsn = "";
-let collection = "";
 
-function getdbfromEnv() {
-    if (process.env.NODE_ENV === 'test') {
-        dsn = "mongodb://localhost:27017/testdocs";
-        collection = "testcollection";
-    } else if (process.env.NODE_ENV === 'integration-test') {
-        dsn = "mongodb://localhost:27017/int-testdocs";
-        collection = "int-testcollection";
-    } else {
-        dsn = `mongodb+srv://${process.env.DB_USER}
-        :${process.env.DB_PASS}
-        @jsramverk.owzo2.mongodb.net/?retryWrites=true&w=majority&appName=jsramverk`;
-        collection = "docscollection";
-    }
-    return [dsn, collection];
-}
-
-/* istanbul ignore end */
 const dbhandler = {
     /**
      * Finds all documents in the collection
      * @returns {Promise<Object[]>}
      */
     findAll: async function findAll() {
-        const client = await mongo.connect(getdbfromEnv()[0]);
+        const client = await mongo.connect(dsn);
         const db = await client.db();
-        const col = await db.collection(getdbfromEnv()[1]);
+        const col = await db.collection(collection);
         const res = await col.find({}).toArray();
 
         await client.close();
@@ -45,9 +27,9 @@ const dbhandler = {
      * @returns {Promise<Object[]> | undefined>}
      */
     findWithId: async function findWithId(id) {
-        const client = await mongo.connect(getdbfromEnv()[0]);
+        const client = await mongo.connect(dsn);
         const db = await client.db();
-        const col = await db.collection(getdbfromEnv()[1]);
+        const col = await db.collection(collection);
         const res = await col.find({ _id: new ObjectId(id) }).toArray();
 
         await client.close();
@@ -62,11 +44,11 @@ const dbhandler = {
      * @returns {Promise<Object>} - The result of the insert operation.
      */
     addDocument: async function addDocument(title, content, shareusername) {
-        const client = await mongo.connect(getdbfromEnv()[0]);
+        const client = await mongo.connect(dsn);
 
         console.log('shareusername:', shareusername);
         const db = await client.db();
-        const col = await db.collection(getdbfromEnv()[1]);
+        const col = await db.collection(collection);
         const doc = { title: title, content: content, sharedWith: ["admin", shareusername] };
         const res = await col.insertOne(doc);
 
@@ -81,9 +63,9 @@ const dbhandler = {
      * @returns {Promise<Object[]> | undefined>}
      */
     deleteWithId: async function deleteWithId(id) {
-        const client = await mongo.connect(getdbfromEnv()[0]);
+        const client = await mongo.connect(dsn);
         const db = await client.db();
-        const col = await db.collection(getdbfromEnv()[1]);
+        const col = await db.collection(collection);
         const res = await col.deleteOne({ _id: new ObjectId(id) });
 
         await client.close();
@@ -99,7 +81,7 @@ const dbhandler = {
      * @returns {Promise<Object>} - The result of the insert operation
      */
     sendUser: async function sendUser(username, email, password) {
-        const client = await mongo.connect(getdbfromEnv()[0]);
+        const client = await mongo.connect(dsn);
         const db = await client.db();
         const col = await db.collection("users");
         const nameentry = { username: username, email: email, password: password };
@@ -110,18 +92,8 @@ const dbhandler = {
         return res;
     },
 
-    /**
-     * Compares the input password with the one stored in the database.
-     * @param {string} username - The username of the user
-     * @param {string} inputpassword - The password that the user is trying to log in with
-     * @returns {Promise<string>} - A string indicating whether the passwords match or not.
-     * "Match" - The passwords match
-     * "No user exists with this username." - The username does not exist in the database
-     * "Wrong password." - The passwords do not match
-     * err.message - An error occurred
-     */
     matchPass: async function matchPass(username, inputpassword) {
-        const client = await mongo.connect(getdbfromEnv()[0]);
+        const client = await mongo.connect(dsn);
         const db = await client.db();
         const col = await db.collection("users");
         let resstring = "";
@@ -149,14 +121,14 @@ const dbhandler = {
     },
 
     /**
-     * Finds a document in the database by its ObjectId, and updates it.
+     * Finds a document in the database by its ObjectId, and updates it or undefined
      * @param {string} id
      * @returns {Promise<Object[]> | undefined>}
      */
     updateDocument: async function updateDocument(id, title, content) {
-        const client = await mongo.connect(getdbfromEnv()[0]);
+        const client = await mongo.connect(dsn);
         const db = await client.db();
-        const col = await db.collection(getdbfromEnv()[1]);
+        const col = await db.collection(collection);
         const res = await col.updateOne({ _id: new ObjectId(id) },
             { $set: { title: title, content: content } });
 
