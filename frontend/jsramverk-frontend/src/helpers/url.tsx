634cf6fcee10bc64b46df3c32dcef5a5feb72671
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2024-10-29 17:20:17 +0100

    Added test case for backend and updated to userhandler

--- frontend/jsramverk-frontend/src/helpers/url.tsx
+++ frontend/jsramverk-frontend/src/helpers/url.tsx
@@ -5,7 +5,10 @@ if (process.env.NODE_ENV === 'dev') {
    url = "http://localhost:3539";
 } else if (process.env.NODE_ENV === 'integration-test') {
    url = "http://localhost:3539"
-} else {
+} else if (process.env.NODE_ENV === 'test') {
+   url = "http://localhost:3539"
+} 
+else {
    url = "https://jsramverk-eafmccbgceegf9bt.swedencentral-01.azurewebsites.net"
 }
 
